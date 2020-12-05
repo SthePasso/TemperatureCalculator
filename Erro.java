934d3f567ecc795ff94f430c0d99b6c95dd38315
Sthefanie Passo <46830594+SthePasso@users.noreply.github.com>
2020-12-05 18:54:23 -0400

    Error File

---
+++ Erro.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Erro
+{
+	public static void main(String arg[])	{
+	    double fahrenheit,c;
+      //Scanner f = new Scanner(System.in);	   	 
+	    //System.out.println("Enter  Fahrenheit temperature");
+      	fahrenheit = 99//f.nextDouble();
+	    
+      //FahrenheittoCelsius fah=new FahrenheittoCelsius( );
+	    
+      c = (fahrenheit-32)*5/9;	
+	    System.out.println("Celsius temperature is = "+c);		  	  	     
+	} 	
+}
