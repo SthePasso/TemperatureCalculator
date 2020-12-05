895d5b7c4e4a74f16a288686f90b45bc91e65f53
Sthefanie Passo <46830594+SthePasso@users.noreply.github.com>
2020-12-05 18:51:41 -0400

    Take out Scanner

--- Main.java
+++ Main.java
@@ -3,13 +3,13 @@ class Main
 {
 	public static void main(String arg[])	{
 	    double fahrenheit,c;
-      Scanner f = new Scanner(System.in);	   	 
-	    System.out.println("Enter  Fahrenheit temperature");
-      fahrenheit = f.nextDouble();
+      //Scanner f = new Scanner(System.in);	   	 
+	    //System.out.println("Enter  Fahrenheit temperature");
+      	fahrenheit = 99;//f.nextDouble();
 	    
       //FahrenheittoCelsius fah=new FahrenheittoCelsius( );
 	    
-      double result = (fahrenheit-32)*5/9;	
-	    System.out.println("Celsius temperature is = "+result);		  	  	     
+      c = (fahrenheit-32)*5/9;	
+	    System.out.println("Celsius temperature is = "+c);		  	  	     
 	} 	
 }
