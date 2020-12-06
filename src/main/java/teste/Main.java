c2d4e4c03526090751488883783e8b222b0784e2
SthePasso <sjgp@icomp.ufam.edu.br>
2020-12-05 19:35:52 -0600

    With Maven

---
+++ src/main/java/teste/Main.java
@@ -0,0 +1,12 @@
+package teste;
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String arg[])	{
+	    double fahrenheit,c;
+      	fahrenheit = 99;//f.nextDouble();
+	    	    
+      	c = (fahrenheit-32)*5/9;	
+	    System.out.println("Celsius temperature is = "+c);		  	  	     
+	} 	
+}
---
+++ src/test/java/teste/MainTest.java
@@ -0,0 +1,17 @@
+package teste;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    
+    public static final double VALOR_ESPERADO = 10.0;
+
+    @Test
+    public void farineitToCelcius(){
+        double fahrenheit = 50.0;
+        double valor = (fahrenheit-32)*5/9;
+        assertEquals(VALOR_ESPERADO, valor);
+    }
+}
