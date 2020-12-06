e038d5cd76957793f706cb6af92a704c4d0a6ed4
Sthefanie Passo <46830594+SthePasso@users.noreply.github.com>
2020-12-05 20:02:46 -0400

    Test 1

--- src/main/java/teste/Main.java
+++ src/main/java/teste/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String arg[])	{
 	    double fahrenheit,c;
-      	fahrenheit = 99//f.nextDouble();
+      	fahrenheit = 99;//f.nextDouble();
 	    	    
       	c = (fahrenheit-32)*5/9;	
 	    System.out.println("Celsius temperature is = "+c);		  	  	     
